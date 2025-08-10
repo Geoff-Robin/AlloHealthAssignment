6437a18bdf0dc418714120173c9ab628b0b9c5c8
Geoff-Robin <jeffrobin132004@gmail.com>
2025-08-10 18:26:51 +0530

    updated doctor resource dto

--- backend/src/doctor/dto/create-doctor.dto.ts
+++ backend/src/doctor/dto/create-doctor.dto.ts
@@ -1,20 +1,23 @@
-import { IsString } from "class-validator";
-
+import { IsString, IsNotEmpty, IsIn, Matches } from "class-validator";
 
 export class CreateDoctorDto {
   @IsString()
+  @IsNotEmpty()
   name: string;
 
   @IsString()
+  @IsNotEmpty()
   specialization: string;
 
   @IsString()
+  @IsIn(['male', 'female', 'other'])
   gender: string;
 
   @IsString()
+  @Matches(/^([0-1]\d|2[0-3]):([0-5]\d)$/, { message: 'startTime must be in HH:mm format' })
   startTime: string;
 
   @IsString()
+  @Matches(/^([0-1]\d|2[0-3]):([0-5]\d)$/, { message: 'endTime must be in HH:mm format' })
   endTime: string;
-
 }
